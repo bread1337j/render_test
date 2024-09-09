244094f17828ec11e1b21ae2d1fca39271213aab
agaziiantc <agaziiantc60@stuy.edu>
2024-09-08 20:12:14 -0400

    added a horizontal line function, but it's flickery. It can be solved by changing paint(Graphics g) to paintComponents(Graphics g) but I am going to deal with that tomorrow

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import org.w3c.dom.css.RGBColor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Collections;
 import java.util.Random;
 import java.util.SplittableRandom;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     static SplittableRandom rand = new SplittableRandom();
-    static byte[][] bytearrpow2 = new byte[250001][3];
+    static byte[][] bytearrpow2 = new byte[255511][3];
+    static byte[] BLACK = new byte[]{(byte)0, (byte)0, (byte)0};
     public static void addbytearr(byte[] b, int loc){
         bytearrpow2[loc] = b;
     }
@@ -20,42 +22,86 @@ public class Main {
     }
     public static void RandomStuffGo(){
         for(int i=0; i<250001; i++){
-            addbytearralt((byte)rand.nextInt(127),(byte)rand.nextInt(127), (byte)rand.nextInt(127),i);
+            //int randint = (byte)rand.nextInt(127);
+            //addbytearralt((byte) (randint+20),(byte) (randint-20),(byte) randint,i);
+            addbytearralt((byte)rand.nextInt(127),(byte) (byte)rand.nextInt(127),(byte) (byte)rand.nextInt(127),i);
         }
+    }
+    public static void FillScreen(byte[] clr){
+        for (int y = 0; y <= 500; y++) {
+            for (int x = 0; x <= 500; x++) {
+                addbytearr(clr, x+y*500);
+            }
+            //System.out.println(y);
+        }
+    }
+    public static void AddLineHorizontal(int x1, int x2, int y, byte[] clr){
+        int h = y * 500;
+        for(int i=x1; i<=x2; i++){
+            addbytearr(clr, h+i);
+            //System.out.println("x coords: " + i + " y coords: " + h + " || loc: " + (i + h));
+
+        }
+
+    }
+
+    public static void AddRect(int x1,int y1,int x2,int y2){
+
     }
     public static void RenderByteArr(byte[][] arr, Window w){
         w.bytearrpow2 = arr;
         //w.setVisible(false);
         //w.setVisible(true);
-        SwingUtilities.updateComponentTreeUI(w.fr);
+        //SwingUtilities.updateComponentTreeUI(w.fr);
     }
 
     public static void main(String[] args) throws InterruptedException {
         Window bebra = new Window();
         bebra.MakeWindow();
-        //addbytearralt((byte)100, (byte)100, (byte)(100), 1);
+
         //System.out.println(GetColorOfLoc(1));
-        RandomStuffGo();
+        //RandomStuffGo();
         System.out.println("a");
         RenderByteArr(bytearrpow2, bebra);
         System.out.println("a");
         //bebra.paint(bebra.pn.getGraphics());
         bebra.MakeVisible();
         bebra.repaint();
+        AddLineHorizontal(100,200, 200, new byte[]{00,00,100});
+        int ticks = 0;
         while(true){
-            long startTime = System.nanoTime();
+            long Time1 = System.nanoTime();
+            ticks += 1;
+
+            //RandomStuffGo();
+            FillScreen(new byte[]{100,100,100});
+            AddLineHorizontal(100,200, 100, BLACK);
 
 
-            RandomStuffGo();
-            RenderByteArr(bytearrpow2, bebra);
-
-
-            long endTime = System.nanoTime();
+            long Time2 = System.nanoTime();
             long executionTime
-                    = (endTime - startTime) / 1000;
+                    = (Time2 - Time1) / 1000;
+            System.out.print(ticks + " | Generating takes "
+                    + executionTime + "ns" + " | ");
 
+
+
+            RenderByteArr(bytearrpow2, bebra);
+            //System.out.println("Rendered.");
+            //TimeUnit.MILLISECONDS.sleep(5);
+            //bebra.fr.getContentPane().repaint();
+            //bebra.pn.repaint();
+            //System.out.println("Displayed.");
+            //TimeUnit.SECONDS.sleep(1);
+            SwingUtilities.updateComponentTreeUI(bebra.fr);
+            //TimeUnit.SECONDS.sleep(1);
+
+
+            long Time3 = System.nanoTime();
+            long executionTime2
+                    = (Time3 - Time2) / 100;
             System.out.println("Rendering takes "
-                    + executionTime + "ns" + " | " + 1000000/executionTime + " FPS");
+                    + executionTime2 + "t" + " | " + 10000000/executionTime2 + " FPS");
 
             //TimeUnit.SECONDS.sleep(1);
             //bebra.repaint();
--- src/Window.java
+++ src/Window.java
@@ -6,15 +6,29 @@ import javax.swing.JComponent;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class Window extends JFrame {
-    JPanel pn = new JPanel();
+public class Window extends JPanel {
+    JPanel pn = new JPanel() {@Override
+    public void paintComponents(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        //g2d.drawRect(100, 100, 10, 10);
+        for (int y = 0; y <= 500; y++) {
+            for (int x = 0; x <= 500; x++) {
+                g2d.setColor(GetColorOfLoc(x+y*500));
+                g2d.fillRect(x*4, y*4, 4, 4);
+
+            }
+            System.out.println(y);
+        }
+        //System.out.println("Render complete");
+
+    }};
     JFrame fr = new JFrame() {@Override
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         //g2d.drawRect(100, 100, 10, 10);
         for (int y = 0; y <= 500; y++) {
             for (int x = 0; x <= 500; x++) {
-                g2d.setColor(GetColorOfLoc(x*y));
+                g2d.setColor(GetColorOfLoc(x+y*500));
                 g2d.fillRect(x*4, y*4, 4, 4);
 
             }
@@ -23,6 +37,8 @@ public class Window extends JFrame {
         //System.out.println("Render complete");
 
     }};
+    ;
+
     static byte[][] bytearrpow2 = new byte[250001][3];
     public void MakeWindow() {
         fr.setSize(600,600);
